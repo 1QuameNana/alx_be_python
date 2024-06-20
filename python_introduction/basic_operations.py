fec9f00777179cb7ffa47cb54056c0581030974f
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-06-20 01:48:48 +0100

    fixed error in code

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,16 @@
 number1 = 10
 number2 = 5
-summ = number1 + number2
-difference = number1 - number2
-product = number1 * number2
-print('addition of 10 and 5 is' summ)
-print('subtraction of 10 and 5 is' difference)
-print('Multiplication of 10 and 5 is' product)
+add= number1 + number2
+sub = number1 -number2
+Mult = number1 * number2
+print('Additon of 10 and 5 is ' , add)
+print('Subtraction of 10 and 5 is ' , sub)
+print('Multiplication of 10 and 5 is ' , Mult)
+
+
+
+
+
+
+
+
