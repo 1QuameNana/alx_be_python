49342a40545abfc3e77d70d51f188fb5c8b5ccea
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-06-20 01:33:54 +0100

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+summ = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+print('addition of 10 and 5 is' summ)
+print('subtraction of 10 and 5 is' difference)
+print('Multiplication of 10 and 5 is' product)
