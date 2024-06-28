6aa6573eed5f68457d6f3e17f342eb43a81bfbcf
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-06-28 02:04:35 +0100

    finance_calculator.py

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,13 @@
+weather = input('What is the weather like today?(sunny/rainy/cold):').lower()
+if weather == 'sunny':
+    print('wear a t-shirt and sunglasses.')
+
+elif weather == 'rainy':
+        print("Don't forget your umbrella and a  raincoat")
+
+elif weather == 'cold':
+   print('Make sure to wear a warm coat and a scarf')
+
+else:
+    print("sorry, I don't have recommendations for this weather")
+
