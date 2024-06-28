e570512eb67c1aa31b142f6446121bc4d045d9c6
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-06-28 02:09:28 +0100

    error ommision

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,4 +4,3 @@ Monthly_savings = Monthly_income - Monthly_expenses
 Projected_savings = Monthly_savings*12 + (Monthly_savings*12*0.05)
 print ('your monthly savings are ', '$' + str(Monthly_savings))
 print('Projected savings after on year, with interest, is:', '$'+str(Projected_savings))
-
