aea95bc8db960dcb05c905c90a8a88ef30a19b55
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-06-28 02:22:51 +0100

    weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -9,5 +9,5 @@ elif weather == 'cold':
    print('Make sure to wear a warm coat and a scarf')
 
 else:
-    print("sorry, I don't have recommendations for this weather")
+    print("Sorry, I don't have recommendations for this weather")
 
