c9af273ddb111cfbb7fd26f69b1f1cd7d7e9a294
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-06-29 03:41:00 +0100

    dialy_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -8,17 +8,17 @@ Priority = input('Priority (high/medium/low):')
 match Priority:
     case 'high':
         if Time_bound =='yes': 
-            print(f'{Task} is a {Priority} priority task that requires immediate attention')
+            print(f' Reminder:{Task} is a {Priority} priority task that requires immediate attention')
     case 'medium':
         if Time_bound =='yes':
-            print(f'{Task} is a {Priority} priority task and requires your seious attention')
+            print(f'Reminder:{Task} is a {Priority} priority task and requires your seious attention')
         else:
-            print(f'{Task} is a {Priority} priority task with no time bound therefore requires some level attention')
+            print(f'REminder:{Task} is a {Priority} priority task with no time bound therefore requires some level attention')
     case 'low':
         if Time_bound =='no':
-            print(f'{Task} is a {priority} task. Consider completing it when you have free time')
+            print(f'Note:{Task} is a {priority} task. Consider completing it when you have free time')
         else:
-            print(f'{Task} is a {Priority} priority task with time bound therefore requires some level attention')
+            print(f'Note:{Task} is a {Priority} priority task with time bound therefore requires some level attention')
             
     case _:
-        print('Relax, there are no important tasks for you today')
+        print('Note: Relax, there are no important tasks for you today')
