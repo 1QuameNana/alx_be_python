77572436a5c31909339d3bf23574afd5453a8127
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-06-29 03:11:45 +0100

    daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,20 +1,20 @@
-priority = input(' Priority high/medium/low:').lower()
-task = input('Enter your task:').lower()
-time_bound =input('Is it time bound?:').lower()
-match priority:
+Priority = input(' Priority high/medium/low:').lower()
+Task = input('Enter your task:').lower()
+Time_bound =input('Is it time bound?:').lower()
+match Priority:
     case 'high':
-        if time_bound =='yes': 
-            print(f'{task} is a {priority} priority task that requires immediate attention')
+        if Time_bound =='yes': 
+            print(f'{Task} is a {Priority} priority task that requires immediate attention')
     case 'medium':
         if time_bound =='yes':
-            print(f'{task} is a {priority} priority task and requires your seious attention')
+            print(f'{Task} is a {Priority} priority task and requires your seious attention')
         else:
-            print(f'{task} is a {priority} priority task with no time bound therefore requires some level attention')
+            print(f'{Task} is a {Priority} priority task with no time bound therefore requires some level attention')
     case 'low':
-        if time_bound =='no':
-            print(f'{task} is a {priority} task. Consider completing it when you have free time')
+        if Time_bound =='no':
+            print(f'{Task} is a {priority} task. Consider completing it when you have free time')
         else:
-            print(f'{task} is a {priority} priority task with time bound therefore requires some level attention')
+            print(f'{Task} is a {Priority} priority task with time bound therefore requires some level attention')
             
     case _:
         print('Relax, there are no important tasks for you today')
