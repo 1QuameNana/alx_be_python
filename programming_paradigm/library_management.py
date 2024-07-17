f71de6399ac3fcb2d14e828567011f2b7c56bb99
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-07-17 01:36:57 +0100

    libray_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,41 +1,66 @@
 class Book:
-    def __init__(self, title, author, is_checked_out=False):
+    """creates a book class with attributes title author and also checks
+    if the book is checked out or is available for borrowing
+    """
+    def __init__(self, title,author):
         self.title = title
-        self.author = author
-        self.is_checked_out = is_checked_out
+        self.author =author
+        self.is_checked_out = False
+        
     def return_book(self):
-        return self.title 
-    def is_checked_out(self):
-        return self.is_checked_out
-
-    def __str__(self):
-        return f"{self.book_id}: {self.title} by {self.author}, {'Checked out' if self.is_checked_out else 'Available'}"
+        """ This is a method to return borrowed books by using the checkout 
+        attribute
+        """
+        if self.is_checked_out == False:
+            return'book available'
+        else:
+            return 'book returned successfully'
+        
+    def is_check_out(self):
+        """ checks to see a title is checked"""
+        if self.is_check_out ==True:
+            return f'{self.title} is checked out'
+        else:
+            return f'{self.title} is available for borrowing '
+         
     
-class Library:
+    
+class library:
+    """ Creates a library to add, check out, and return borrowed books by 
+    referencing their titles
+    """
     def __init__(self):
         self._books = []
-
+        
     def add_book(self, title):
-        if title in self._books:
-            return("This book ID already exists.")
-        else:
-            self._books.title
-            return("Book added successfully.")
-    def check_out_book(self, title ):
+        """ adds books to the list _books by title referencing"""
         if title not in self._books:
-            return("Book not found.")
-        elif self._books[title].is_checked_out:
-            return("Book is already checked out.")
+            self._books.append(title)
+        
+        
+    def is_checked_out(self, title):
+        """ A method to check out a book by referencing the title fromt the list
+        _books"""
+        if title not in self._books:
+            return 'book not found'
+        elif title.self.is_checked_out:
+            return f'{title} is already'     
         else:
-            self._books[title].is_checked_out = True
-            return("Book checked out successfully.")  
+            title.self.is_checked_out = True
+        
+            return f'{title} is checked out successfully'
+        
     def return_book(self, title):
-        if title not in self._books:
-            return("Book not found.")
-        elif not self._books[title].is_checked_out:
-            return("Book is not checked out.")
+        """Returns borrowed books back to the list _books"""
+        if title in self._books:
+            
+            return f'{title} is available'
         else:
-            self._books[title].is_checked_out = False
-            return("Book checked in successfully.")
-    def list_available_books(self):
-        return self._books
+            
+            return f'{title} is returned successfully'
+        
+    def listavailablebooks(self):
+        """ Prints the available books for viewing"""
+        for book in self._books: 
+            print(book)
+
