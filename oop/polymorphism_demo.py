b9557d18b8d3310f304c076bfcdfb51ca089ed6b
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-07-21 11:22:39 +0100

    polymorphism_demo.py

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,25 @@
+
+import math as mt
+
+
+class Shape:
+    def __init__(self):
+        pass
+    def area (self):
+        pass
+class Rectangle(Shape):
+    def __init__(self,length,width):
+         self.length = length
+         self.width = width
+
+    def area(self):
+        return self.length* self.width
+    
+class circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area (self):
+        return mt.pi*(self.radius**2)
+        
+  
--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,64 +1,41 @@
 class Book:
-    """creates a book class with attributes title author and also checks
-    if the book is checked out or is available for borrowing
-    """
-    def __init__(self, title,author):
+    def __init__(self, title, author, is_checked_out=False):
         self.title = title
-        self.author =author
-        self.is_checked_out = False
-        
+        self.author = author
+        self.is_checked_out = is_checked_out
     def return_book(self):
-        """ This is a method to return borrowed books by using the checkout 
-        attribute
-        """
-        if self.is_checked_out == False:
-            return'book available'
-        else:
-            return 'book returned successfully'
-        
-    def is_check_out(self):
-        """ checks to see a title is checked"""
-        if self.is_check_out ==True:
-            return f'{self.title} is checked out'
-        else:
-            return f'{self.title} is available for borrowing '
-            
-class Library:
-    """ Creates a library to add, check out, and return borrowed books by 
-    referencing their titles
-    """
-    def __init__(self):
-        self._books = []
-        
-    def add_book(self, title):
-        """ adds books to the list _books by title referencing"""
-        if title not in self._books:
-            self._books.append(title)
-        
-        
-    def is_checked_out(self, title):
-        """ A method to check out a book by referencing the title fromt the list
-        _books"""
-        if title not in self._books:
-            return 'book not found'
-        elif title.self.is_checked_out:
-            return f'{title} is already'     
-        else:
-            title.self.is_checked_out = True
-        
-            return f'{title} is checked out successfully'
-        
-    def return_book(self, title):
-        """Returns borrowed books back to the list _books"""
-        if title in self._books:
-            
-            return f'{title} is available'
-        else:
-            
-            return f'{title} is returned successfully'
-        
-    def list_available_books(self):
-        """ Prints the available books for viewing"""
-        for book in self._books: 
-            print(book)
+        return self.title 
+    def is_checked_out(self):
+        return self.is_checked_out
 
+    def __str__(self):
+        return f"{self.title} by {self.author}, {'Checked out' if self.is_checked_out else 'Available'}"
+
+class Library(Book):
+    def __init__(self, title, author, is_checked_out=False, ):
+        super.__init__(self, title, author, is_checked_out=False)
+        self._books = []
+
+    def add_book(self, title):
+        if title in self._books:
+            return("This title already exists.")
+        else:
+            self._books.append(title)
+    def check_out_book(self, title):
+        if title not in self._books:
+            return("Book not found.")
+        elif self._books[title].self.is_checked_out:
+            return("Book is already checked out.")
+        else:
+            self._books[title].self.is_checked_out = True
+            return("Book checked out successfully.")  
+    def return_book(self, title):
+        if title not in self._books:
+            return("Book not found.")
+        elif not self._books[title].self.is_checked_out:
+            return("Book is not checked out.")
+        else:
+            self._books[title].self.is_checked_out = False
+            return("Book checked in successfully.")
+        def list_availabele_books(self):
+            return(self._books)
