b745afae02006c753d815ac71b509a05161a6dc5
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-07-28 16:23:49 +0100

    alx_book_store.sql

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,11 +1,11 @@
-class Book: #base class Book
+class Book: 
     def __init__(self,title,author):
         self.title = title
         self.author = author
     def __str__(self):return f'Book: {self.title} by {self.author}'
     
         
-class EBook(Book): #child class Ebook
+class EBook(Book): 
     
     def __init__(self,title,author,file_size:int):
         super().__init__(title,author)
@@ -15,7 +15,7 @@ class EBook(Book): #child class Ebook
         return f'EBook: {self.title} by {self.author}, File Size: {str(self.file_size)}KB'
     
     
-class PrintBook(Book):
+class PrintBook(Book):    
     def __init__(self,title,author,page_count:int):
         super().__init__(title,author)
         self.page_count = page_count
